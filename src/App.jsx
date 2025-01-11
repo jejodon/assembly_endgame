3fbb4764a6302bcd5d3194ea2d665e674af77c76
JesseJojo <jessedonkor21@gmail.com>
2025-01-11 12:13:21 +0000

    build main content

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,46 @@
+import { useState } from "react"
+import { languages } from "./languages"
+
 export default function App() {
-  return (
-    <main>
-      <header>
-        <h1>Assembly: Endgame</h1>
-        <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
-      </header>
-    </main>
-  )
+    const [currentWord, setCurrentWord] = useState("react")
+
+    const languageElements = languages.map(lang => {
+        const styles = {
+            backgroundColor: lang.backgroundColor,
+            color: lang.color
+        }
+        return (
+            <span
+                className="chip"
+                style={styles}
+                key={lang.name}
+            >
+                {lang.name}
+            </span>
+        )
+    })
+    
+    const letterElements = currentWord.split("").map((letter, index) => (
+        <span key={index}>{letter.toUpperCase()}</span>
+    ))
+
+    return (
+        <main>
+            <header>
+                <h1>Assembly: Endgame</h1>
+                <p>Guess the word within 8 attempts to keep the
+                programming world safe from Assembly!</p>
+            </header>
+            <section className="game-status">
+                <h2>You win!</h2>
+                <p>Well done! 🎉</p>
+            </section>
+            <section className="language-chips">
+                {languageElements}
+            </section>
+            <section className="word">
+                {letterElements}
+            </section>
+        </main>
+    )
 }
